b1962aad34113ac4712e8fe284b254f801673a5a
sstkachev <sstkachev@gmail.com>
2022-09-07 15:45:50 +0300

    two empty files

---
+++ lib/beer_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BeerList extends StatefulWidget {
+  const BeerList({Key? key}) : super(key: key);
+
+  @override
+  State<BeerList> createState() => _BeerListState();
+}
+
+class _BeerListState extends State<BeerList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
